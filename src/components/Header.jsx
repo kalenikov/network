a9292ece017a025efb30c3bfcaa939e73956132e
Sergey Kalenikov <sergeykalenikov@gmail.com>
2020-04-05 20:07:48 +0300

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className={'header'}>
+        <img src='https://i.pinimg.com/736x/5a/ae/50/5aae503e4f037a5a4375944d8861fb6a.jpg'/>
+        Header
+    </header>
+}
+
+export default Header
